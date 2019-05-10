87c1c7787268ae14f13f35c4a2bf563faae191d3
jiaccanche <jcanchecauich@gmail.com>
2019-05-10 18:33:43 -0500

    Se ha agregado la clase de conection para hacer la base de datos, también se puso un ejemplo para hacer un query simple y se vea el funcionamiento del pdo

---
+++ Clases/conection.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jorge canche
+ * Date: 10/05/2019
+ * Time: 04:37 PM
+ */
+
+class conection
+{
+
+    private $server = "localhost";
+    private $password = "12345";
+    private $user = "postgres";
+    private $bdname = "SAES";
+    private $conection_bd = null;
+
+    public function __construct()
+    {
+    }
+
+    public function conectar($nameBD=null){
+
+        if($nameBD !=null){
+            return;
+        }else{
+
+            $dsn = "pgsql:host=$this->server;port=5432;dbname=$this->bdname";
+            try{
+                /*Se crea una conexión*/
+                $this->conection_bd = new PDO($dsn,$this->getUser(),$this->getPassword());
+
+                if($this->conection_bd){
+                    /*La conexión fue correcta*/
+                    return $this->conection_bd;
+                }
+            }catch (PDOException $e) {
+                // report error message
+                die($e->getMessage());
+            }
+        }
+
+
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param mixed $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param mixed $user
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBdname()
+    {
+        return $this->bdname;
+    }
+
+    /**
+     * @param mixed $bdname
+     */
+    public function setBdname($bdname)
+    {
+        $this->bdname = $bdname;
+    }
+
+    /**
+     * @return resource|null
+     */
+    public function getConectionBd()
+    {
+        return $this->conection_bd;
+    }
+
+    /**
+     * @param resource|null $conection_bd
+     */
+    public function setConectionBd($conection_bd)
+    {
+        $this->conection_bd = $conection_bd;
+    }
+    /**
+     * @return mixed
+     */
+    public function getServer()
+    {
+        return $this->server;
+    }
+
+    /**
+     * @param mixed $server
+     */
+    public function setServer($server)
+    {
+        $this->server = $server;
+    }
+}
+
+
+
+    //Ejemplo para realizar la conección y hacer un query simple para hacer un select
+    /*
+    //crear el objeto para poder realizar la conexion
+    $objeto_to_conectar = new conection();
+    //Haces la conexión
+    $pd = $objeto_to_conectar->conectar();
+    //Realizar una preparación para un query
+    $registrosx = $pd->prepare("select * from empleado");
+    //Ejecutas el query preparado
+    $registrosx->execute();
+    //obtienes los datos extraidos, aqui puedes crear objeto tipo std de php
+    $lista = $registrosx->fetchAll(PDO::FETCH_ASSOC);
+    var_dump($lista);*/
+
+
+
+
+
+
--- index.php
+++ index.php
@@ -1,3 +1,44 @@
 <?php
-    echo "Hola saes!";
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" ></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" ></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" ></script>
+    <title>Entradas/Usuario</title>
+</head>
+<body>
+<div class="container">
+		<div class="d-flex justify-content-center" style="border:1px solid blue;padding-top: 100px; padding-bottom: 20px">
+			<div class="user_card border">
+				<div class="d-flex justify-content-center">
+				</div>
+				<div class="d-flex justify-content-center form_container">
+					<form>
+						<div class="input-group mb-3">
+							<div class="input-group-append">
+								<span class="input-group-text"><i class="fas fa-user"></i></span>
+							</div>
+							<input type="text" name="" class="form-control input_user" value="" placeholder="username" required>
+						</div>
+						<div class="input-group mb-2">
+							<div class="input-group-append">
+								<span class="input-group-text"><i class="fas fa-key"></i></span>
+							</div>
+							<input type="password" name="" class="form-control input_pass" value="" placeholder="password" required>
+						</div>
+					</form>
+				</div>
+				<div class="d-flex justify-content-center mt-3 login_container">
+					<button class="btn btn-outline-success" type="button" name="button" class="btn login_btn">Aceptar</button>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
